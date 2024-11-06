f9fcc9b6c3ab7d3162187388c18d09facb2b37f5
Ashish Singh <singhashish82103@gmail.com>
2024-11-06 11:35:15 +0530

    Initial commit

--- Lect_1/script.js
+++ Lect_1/script.js
@@ -582,7 +582,30 @@ const firstTrough = numbers
   });
 console.log(firstTrough); // 1
 
+const array1 = [5, 12, 8, 130, 44];
 
+const isLargeNumber = (element) => element > 13;
+
+console.log(array1.findIndex(isLargeNumber));
+// Expected output: 3
+
+findIndex(callbackFn)
+findIndex(callbackFn, thisArg)
+
+function isPrime(element) {
+    if (element % 2 === 0 || element < 2) {
+      return false;
+    }
+    for (let factor = 3; factor <= Math.sqrt(element); factor += 2) {
+      if (element % factor === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  console.log([4, 6, 8, 9, 12].findIndex(isPrime)); // -1, not found
+  console.log([4, 6, 7, 9, 12].findIndex(isPrime)); // 2 (array[2] is 7)
 
 
 
