d4aa060d11fc1c9b19df2b5e4419c6849a41be68
Ashish Singh <singhashish82103@gmail.com>
2024-11-07 14:47:21 +0530

    Initial commit

--- Lect_1/script.js
+++ Lect_1/script.js
@@ -702,6 +702,51 @@ let promise = new Promise(() => {
 })
 
 
+// This script exists to workaround create-react-app's limitation around importing outside /src
+// Once this moves out of docs/beta, we could just drive readme generation out of docs/src/data instead of docs/data and not copy
+
+import { copyFile, mkdir } from 'fs/promises'
+
+let run = async () => {
+  await mkdir('./docs/beta/src/data', { recursive: true })
+  await copyFile('./docs/data/docs.json', './docs/beta/src/data/docs.json')
+  await copyFile(
+    './docs/data/tag-docs.json',
+    './docs/beta/src/data/tag-docs.json'
+  )
+  await copyFile('./docs/data/tests.json', './docs/beta/src/data/tests.json')
+}
+
+run();
+
+
+import { mkdir } from 'fs/promises'
+
+let run1 = () => mkdir('./docs/data', { recursive: true })
+
+run1();
+
+nextTick.js = new_userbid(() => {
+    new promise(() => {
+        setImmediate(() => {
+            let user = null;
+            console.log(user);
+        }); 
+
+        if(item_user === null) {
+            console.log(loginId);
+        }
+            else if (user !== null) {
+                console.log('login-id');
+            }
+        })
+    
+})
+
+
+
+
+
 
   
 
