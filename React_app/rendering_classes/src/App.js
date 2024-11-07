f77ab2e5449f3d1bdfad0fef2750ba364592f7cc
Ashish Singh <singhashish82103@gmail.com>
2024-11-07 11:22:06 +0530

    Initial commit

--- React_app/rendering_classes/src/App.js
+++ React_app/rendering_classes/src/App.js
@@ -24,7 +24,8 @@ function App() {
 
         <aside src = './stylesheet'>relm stylesheet</aside>
         <aside src = './stylesheet'>relem the main side of the main target of the main side</aside>
-        
+        <p>print the meen value</p>
+        <p>print the value</p>
         <a
           className="App-link"
           href="https://reactjs.org"
