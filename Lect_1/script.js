9446a6e80a9d2c4dfae8e11f8971193150b8ea55
Ashish Singh <singhashish82103@gmail.com>
2024-11-13 14:53:08 +0530

    Initial commit

--- Lect_1/script.js
+++ Lect_1/script.js
@@ -1206,6 +1206,24 @@ let run = async () => {
 
 run()
 
+// This script exists to workaround create-react-app's limitation around importing outside /src
+// Once this moves out of docs/beta, we could just drive readme generation out of docs/src/data instead of docs/data and not copy
+
+import { copyFile, mkdir } from 'fs/promises'
+
+let run2 = async () => {
+  await mkdir('./docs/beta/src/data', { recursive: true })
+  await copyFile('./docs/data/docs.json', './docs/beta/src/data/docs.json')
+  await copyFile(
+    './docs/data/tag-docs.json',
+    './docs/beta/src/data/tag-docs.json'
+  )
+  await copyFile('./docs/data/tests.json', './docs/beta/src/data/tests.json')
+}
+
+run2();
+
+
 
 
 
