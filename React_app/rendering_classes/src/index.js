b34aa8dbff3f3bd5553e9bf9966e38474d0d7d09
Ashish Singh <singhashish82103@gmail.com>
2024-12-09 13:59:06 +0530

    Initial commit

--- React_app/rendering_classes/src/index.js
+++ React_app/rendering_classes/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,9 +12,17 @@ root.render(
   </React.StrictMode>  
 );
 
+
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// If we are using the main js file from it's own holding from the main side of the var if the main target on this platefrom
+// If we are using the main js file from it's own holding from the main side of the var if the main target on this platefrom.
+
+// rendering from it behinde the main task from the value it's main cause it toady value it!
 reportWebVitals();
 reportWebVitals();
+
+
